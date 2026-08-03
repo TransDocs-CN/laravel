efb473b38bf4dcc72fa64b85f58ea85445871fc4
tangzhangming <mr.tangzhangming@gmail.com>
2026-08-03 16:47:38 +0800

    跨站导航

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import SiteTopBar from './components/SiteTopBar.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #layout-top>
+      <SiteTopBar current-key="laravel" />
+    </template>
+  </Layout>
+</template>
